457adae15a99e30647de7bcf35e6c37eef993d6a
Pierre Emmanuel Tanguy <pierre.emmanuel.tanguy@gmail.com>
2020-02-05 02:31:45 +0000

    edits

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+  // import "./pong.js";
+import "./sphere.js";
+// import "./particles.js";
+// import "./brain-cells.js";
+// import "./triangles.js";
+// import "./svg-lines.js";
+
